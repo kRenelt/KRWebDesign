f0c81c89f275d393fe7de847efd870145db6a83b
kRenelt <43582922+kRenelt@users.noreply.github.com>
2019-07-19 12:23:29 -0500

    Add files via upload
    First commit base files with root

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using KRWebDesign.Models;
+
+namespace KRWebDesign
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<UserContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+$(function () {
+   // alert("Hello");
+
+   // $(selector).action();
+
+    // $('#addToCart').hide();
+
+    $("#button").click(function () {
+        $("#test").load("FakeData.txt");
+    });
+
+    $('#customers .jsDelete').click(function () {
+        if (confirm("are you sure you want to delete this Item?")) {
+            $.ajax({
+                url: "/shoppingCart" + $(this).attr("data-item-id"),
+                method: "DELETE",
+                success: function () {
+                    console.log("Success");
+                }
+            });
+        }
+    })
+
+   // $("button").click(function () {
+   //     $('#cartCount').
+   // });
+});
